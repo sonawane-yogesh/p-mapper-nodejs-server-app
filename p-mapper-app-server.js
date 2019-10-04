dc382fe1f48520a2e548966f87442a697afe02d7
sonawane-yogesh <sonawane-yogesh@users.noreply.github.com>
2019-10-04 13:26:20 +0530

    upload changes

--- p-mapper-app-server.js
+++ p-mapper-app-server.js
@@ -4,6 +4,9 @@ var expressPath = require('express-path');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var appRoutes = require('./routes/app-routes');
+const path = require("path");
+const fs = require("fs");
+
 app.use(bodyParser.json({
     limit: '60mb'
 }));
@@ -12,8 +15,31 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cors());
+
 expressPath(app, appRoutes);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+var multiparty = require("multiparty");
+app.post("/submit", function (httpRequest, httpResponse, next) {
+    var dirPath = path.join(__dirname, "./UploadedFiles");
+    if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath);
+
+    var form = new multiparty.Form({
+        uploadDir: "./UploadedFiles"
+    });
+    form.on("part", function (part) {
+        console.log(part.filename);
+        httpResponse.send("Ok");
+    });
+    form.on("file", function (part) {
+        console.log(arguments);
+    });
+    form.on("error", function (error) {
+        console.log(error);
+    })
+    form.parse(httpRequest);
+});
+
 
 app.listen(port, function () {
     console.log(`p-mapper server is running and up on port ${port}`);
