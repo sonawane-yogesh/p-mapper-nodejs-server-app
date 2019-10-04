96a4c087fd0f20fe797ff5df21c31fa00322b08c
sonawane-yogesh <sonawane-yogesh@users.noreply.github.com>
2019-10-04 14:53:13 +0530

    after change 2

--- p-mapper-app-server.js
+++ p-mapper-app-server.js
@@ -7,7 +7,8 @@ var appRoutes = require('./routes/app-routes');
 var axios = require("axios");
 var FormData = require('form-data');
 var fs = require("fs");
-
+var path = require("path");
+var multiparty = require('multiparty');
 app.use(bodyParser.json({
     limit: '60mb'
 }));
@@ -17,18 +18,6 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cors());
 expressPath(app, appRoutes);
-
-// var formData = new FormData();
-const formData = new FormData();
-formData.append("file-upload", fs.createReadStream("./UploadedScripts/sample.pdf"));
-var fileBuffer = fs.readFileSync("./UploadedScripts/sample.pdf"); 
-// formData.append("file-upload", "./UploadedScripts/sample.pdf");       
-axios.post("http://127.0.0.1:3004/api/text-extractor/extract-text", fileBuffer).then(data => {
-    console.log(data);
-}).catch(err => {
-    console.log(err);
-});
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, "127.0.0.1", function () {
--- routes/app-routes.js
+++ routes/app-routes.js
@@ -4,12 +4,9 @@ module.exports = appRoutes = [
     ['api/login-controller/add-user', 'login-controller#addUserDetails', 'post'],
     ['api/login-controller/get-users', 'login-controller#getUserDetails', 'get'],
     ['api/job-card-controller/add-job', 'job-card-controller#addJobCard', 'post'],
-   // ['api/job-card-controller/add-app-card', 'job-card-controller#addApplicationCard', 'post'],
     ['api/job-card-controller/get-predecessor-list', 'job-card-controller#getPredecessor', 'get'],
-    // ['api/job-card-controller/get-app-dependencies', 'job-card-controller#getAppDependancies', 'get'],
     ['api/job-card-controller/get-all-job-cards', 'job-card-controller#getAllJobCards', 'get'],
     ['api/file-upload-controller/upload-file', 'file-upload-controller#uploadAssociatedFile', 'post'],
-    // ['api/job-card-controller/update-app-card', 'job-card-controller#updateApplicationCard', 'post'],
     ['api/job-card-controller/update-job-card', 'job-card-controller#updateJobCard', 'post'],
     ['api/job-card-controller/delete-card', 'job-card-controller#deleteCards', 'delete'],
     ['api/job-card-controller/get-job-cards', 'job-card-controller#getJobCards', 'get'],
@@ -24,3 +21,8 @@ module.exports = appRoutes = [
     ['api/pdf-reader-controller/read-pdf', 'pdf-reader-controller#extractPdf', 'get']
 ];
 
+/*
+ ['api/job-card-controller/update-app-card', 'job-card-controller#updateApplicationCard', 'post'],
+ ['api/job-card-controller/add-app-card', 'job-card-controller#addApplicationCard', 'post'],
+ ['api/job-card-controller/get-app-dependencies', 'job-card-controller#getAppDependancies', 'get'],
+*/
