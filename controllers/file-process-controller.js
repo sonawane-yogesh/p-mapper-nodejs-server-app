bb3a8e0a3c2c91286fc66ea57f068ef8c652a8d4
sonawane-yogesh <sonawane-yogesh@users.noreply.github.com>
2019-10-09 10:54:46 +0530

    file process change

--- controllers/file-process-controller.js
+++ controllers/file-process-controller.js
@@ -1,13 +1,35 @@
 var request = require('request');
+var rp = require('request-promise');
 var fs = require('fs');
-var path = require('path');  
+var path = require('path');
+var {
+    JobCardMaster
+} = require('../model');
 
 var fileProcessing = function () {
+    // Step 1. Collect all files which has not processed or text is not yet extracted...
+    // Like search for IsTextExtracted = false;
+
+    // const someFiles = JobCardMaster.find({
+    //     "AssociatedFiles.IsTextExtracted": false
+    // });
+    // Run array look over someFiles...
+    // for (var file of someFiles) {
+
+    // }
     var formData = {
-        uploadFile: fs.createReadStream(path.join(__dirname, "../UploadedScripts", "2. TFN Information packet.pdf"))
+        uploadFile: fs.createReadStream(path.join(__dirname, "../UploadedScripts", "FileMaster.csv"))
     };
+    // rp.post({
+    //     url: "http://localhost:3001/api/text-extractor/upload-to-process",
+    //     formData: formData
+    // }).then((result) => {
+    //     console.log(result);
+    // }).catch((error) => {
+    //     console.log(error);
+    // });
     request.post({
-        url: "http://localhost:3004/api/text-extractor/upload-to-process",
+        url: "http://localhost:3001/api/text-extractor/upload-to-process",
         formData: formData
     }, function optionalCallback(err, httpResponse, body) {
         if (err) return console.error('upload failed:', err);
@@ -18,4 +40,3 @@ var fileProcessing = function () {
 module.exports = {
     fileProcessing
 };
-
--- p-mapper-app-server.js
+++ p-mapper-app-server.js
@@ -1,12 +1,11 @@
+console.clear();
 var express = require('express');
 var app = express();
 var expressPath = require('express-path');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var appRoutes = require('./routes/app-routes');
-const path = require("path");
-const fs = require("fs");
-var request = require('request');
+
 
 app.use(bodyParser.json({
     limit: '60mb'
@@ -18,16 +17,18 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 
 expressPath(app, appRoutes);
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 // var formData = {
 //     uploadFile: fs.createReadStream(path.join(__dirname, "./UploadedScripts", "sample.pdf"))
 // };
+
 var {
     fileProcessing
 } = require('./controllers/file-process-controller');
 setInterval(function () {
     fileProcessing();
 }, 10000);
+
 // request.post({
 //     url: "http://localhost:3004/api/text-extractor/upload-to-process",
 //     formData: formData
