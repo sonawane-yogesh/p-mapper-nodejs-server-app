4cfd5ed49ade926819b4000d6a2346b6236b23e0
yogeshs@flokapture.solution <Sonawane@#123>
2019-11-07 17:19:03 +0530

    Changes done in user management

--- model/user-master.js
+++ model/user-master.js
@@ -7,7 +7,9 @@ var Schema = mongoose.Schema;
 var {
     RoleMaster
 } = require("./role-master");
-var {ContactTypeMaster} = require("./contact-type-master");
+var {
+    ContactTypeMaster
+} = require("./contact-type-master");
 
 
 
@@ -56,38 +58,36 @@ var roleVirtuals = {
 };
 
 var contactTypeVirtuals = {
-    path:"ContactTypeMaster",
+    path: "ContactTypeMaster",
     value: {
-        from:"ContactTypeMaster",
-        foreignField:"_id",
-        localField:"ContactTypeId",
+        from: "ContactTypeMaster",
+        foreignField: "_id",
+        localField: "ContactTypeId",
         as: "ContactTypeMaster"
     },
     fields: ["_id", "ContactType"]
 }
 
-/*
 var userMasterVirtuals = {
-    path:"UserMaster",
+    path: "UserMaster",
     value: {
         from: "UserMaster",
-        foreignField:"_id",
-        localField:"ReportToId",
-        as:"UserMaster"
+        foreignField: "_id",
+        localField: "ReportToId",
+        as: "ReportsTo"
     },
-    fields: ["_id", "FirstName", "LastName"]
+    fields: ["_id", "FirstName", "LastName", "ContactTypeId"]
 }
-*/
+
 userSchema.virtual(roleVirtuals.path, roleVirtuals.value);
 userSchema.virtual(contactTypeVirtuals.path, contactTypeVirtuals.value);
-// userSchema.virtual(userMasterVirtuals.path, userMasterVirtuals.value);
-
+userSchema.virtual(userMasterVirtuals.path, userMasterVirtuals.value);
 
 const findHook = function (next) {
     var userMaster = this;
     userMaster.populate(roleVirtuals.path, roleVirtuals.fields, RoleMaster);
     userMaster.populate(contactTypeVirtuals.path, contactTypeVirtuals.fields, ContactTypeMaster);
-   //  userMaster.populate(userMasterVirtuals.path, userMasterVirtuals.fields, userMaster);
+    userMaster.populate(userMasterVirtuals.path, userMasterVirtuals.fields);
     next();
 };
 
@@ -97,11 +97,12 @@ userSchema.pre("aggregate", function (next) {
     var userMaster = this;
     userMaster.lookup(roleVirtuals.value).unwind(roleVirtuals.path);
     userMaster.lookup(contactTypeVirtuals.value).unwind(contactTypeVirtuals.path);
+    userMaster.lookup(userMasterVirtuals.value).unwind(userMasterVirtuals.path);
     next();
 });
 
-
 var UserMaster = server.model('UserMaster', userSchema, 'UserMaster');
+
 module.exports = {
     UserMasterSchema: userSchema,
     UserMaster
