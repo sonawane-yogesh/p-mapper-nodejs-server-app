2f6f472b0fa007bf1032718c73268e68d6125af4
yogeshs@flokapture.solution <Sonawane@#123>
2019-11-19 15:32:06 +0530

    chances done in dashboard controller

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,35 +1,47 @@
 var {
     JobCardMaster,
-    MemberReviewMaster,
+    UserMaster,
     ApplicationCardMaster
 } = require("../model/index");
 
 
 module.exports.dashBoardCounts = async function (request, response) {
     var dashBoard = [];
-    await JobCardMaster.count().then((job) => {
-        dashBoard.push({
-            title: "Job Cards",
-            count: job
-        });
-    }).catch((err) => {
-        console.log(err);
+    var job = await JobCardMaster.count();
+    dashBoard.push({
+        title: "Job Cards",
+        count: job
     });
-    await ApplicationCardMaster.count().then((app) => {
-        dashBoard.push({
-            title: "Application Cards",
-            count: app
-        });
-    }).catch((err) => {
-        console.log(err);
+
+    var app = await ApplicationCardMaster.count();
+    dashBoard.push({
+        title: "Application Cards",
+        count: app
     });
-    await MemberReviewMaster.count().then((member) => {
-        dashBoard.push({
-            title: "Members",
-            count: member
-        });
-    }).catch((err) => {
-        console.log(err);
+
+    var user = await UserMaster.count();
+    dashBoard.push({
+        title: "Members",
+        count: user
     });
     response.send(dashBoard);
+};
+
+module.exports.getDistinctApplication = async function (request, response) {
+    var application = [];
+    var result = await ApplicationCardMaster.aggregate([{
+        $group: {
+            _id: "$ServerName"
+        }
+    }]).exec();
+    for (let app of result) {
+        var appCount = await ApplicationCardMaster.count({
+            ServerName: app._id
+        });
+        application.push({
+            "ServerName": app._id,
+            "Count": appCount
+        });
+    }
+    response.send(application);
 }
